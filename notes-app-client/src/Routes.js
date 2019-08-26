e798563bb87a8da386ce953b2298e05417299e66
Rachel Gould <rachel@rachelgould.dev>
2019-08-26 12:15:37 -0700

    add 404 route and view

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from './containers/NotFound';
 
 export default () =>
   // Switch renders the first matching route that is defined within it
   // The `exact` prop makes sure that the URL matches '/' exactly
   <Switch>
     <Route path="/" exact component={Home} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={NotFound} />
   </Switch>;
