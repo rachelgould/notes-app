19c67bad7495590f91507a03fe83d160599d870b
Rachel Gould <rachel@rachelgould.dev>
2019-08-27 15:03:06 -0700

    when component mounts, it retrieves notes from API

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { PageHeader, ListGroup } from "react-bootstrap";
 import './Home.css';
+import { API } from 'aws-amplify';
 
 function Home(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    if (!props.isAuthenticated) {
+      return;
+    }
+
+    const fetchNotes = async () => {
+      try {
+        const notes = await API.get('notes', '/notes');
+        setNotes(notes);
+      } catch (e) {
+        alert(e);
+      }
+    }
+
+    fetchNotes();
+    setIsLoading(false);
+  }, []);
 
   const renderNotesList = notes => {
     return null;
