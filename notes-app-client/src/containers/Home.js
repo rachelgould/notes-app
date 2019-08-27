5d3f3fe2511fe8f3ffb8a02a4886155ab92981b4
Rachel Gould <rachel@rachelgould.dev>
2019-08-27 15:09:01 -0700

    displays list of user's notes

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import './Home.css';
 import { API } from 'aws-amplify';
 
@@ -26,7 +27,33 @@ function Home(props) {
   }, []);
 
   const renderNotesList = notes => {
-    return null;
+    return [{}].concat(notes).map((note, i) => {
+      if (i !== 0) {
+        return (
+          <LinkContainer
+            key={note.noteId}
+            to={`/notes/${note.noteId}`}
+          >
+            <ListGroupItem header={note.content.trim().split('\n')[0]}>
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          </LinkContainer>
+        );
+      } else {
+        return (
+        <LinkContainer
+            key="new"
+            to="/notes/new"
+          >
+            <ListGroupItem>
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
+              </h4>
+            </ListGroupItem>
+          </LinkContainer>
+        );
+      }
+    });
   }
 
   const renderLander = () => {
