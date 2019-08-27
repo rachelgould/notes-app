13584aeaba1ccd86ad08187c3a4ef35abd0635c2
Rachel Gould <rachel@rachelgould.dev>
2019-08-27 14:31:55 -0700

    import usestate

--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
