a5ddd4d394909c48c295804edc39a40a924391b5
Rachel Gould <rachel@rachelgould.dev>
2019-08-27 14:58:32 -0700

    conditionally render if user is authenticated or not

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { PageHeader, ListGroup } from "react-bootstrap";
 import './Home.css';
 
-function Home() {
-  return (
-    <div className="Home">
+function Home(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [notes, setNotes] = useState([]);
+
+  const renderNotesList = notes => {
+    return null;
+  }
+
+  const renderLander = () => {
+    return (
       <div className='lander'>
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
       </div>
+    );
+  }
+
+  const renderNotes = () => {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!isLoading && renderNotesList(notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Home">
+      {props.isAuthenticated ? renderNotes() : renderLander()}
     </div>
   );
 }
